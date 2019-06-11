752190202167d4af90aff57b33c10d1a573604fc
iMacAB <bagga.abdelbasset@gmail.com>
2019-06-11 11:15:14 +0100

    Storing Auth Data in Vuex & Sending the Token to the Backend

--- src/store.js
+++ src/store.js
@@ -1,38 +1,85 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from './axios-auth';
+import globalAxios from 'axios';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    idToken : null,
+    userId : null,
+    user : null
   },
   mutations: {
-
+    authUser(state, userData) {
+      state.idToken = userData.token
+      state.userId = userData.userId
+    },
+    storeUser(state, user){
+      state.user = user;
+    }
   },
   actions: {
-    signup({commit}, authData){
+    signup({commit, dispatch}, authData) {
       axios.post('signupNewUser?key=AIzaSyC31u69mws3DQjoewgC_kmoCw6oPMWaZEA', {
-        email: authData.email,
-        password : authData.password,
-        returnSecureToken : true
-      })
-        .then(res => console.log(res))
+          email: authData.email,
+          password: authData.password,
+          returnSecureToken: true
+        })
+        .then(res => {
+          console.log(res);
+          commit('authUser', {
+            token: res.data.idToken,
+            userId: res.data.localId
+          })
+          dispatch('storeUser', {authData});
+        })
         .catch(error => console.log(error))
     },
-    login({commit}, authData){
+    login({commit}, authData) {
       axios.post('verifyPassword?key=AIzaSyC31u69mws3DQjoewgC_kmoCw6oPMWaZEA', {
-        email: authData.email,
-        password : authData.password,
-        returnSecureToken : true
-      })
-        .then(res => console.log(res))
+          email: authData.email,
+          password: authData.password,
+          returnSecureToken: true
+        })
+        .then(res => {
+           console.log(res);
+           commit('authUser', {
+            token: res.data.idToken,
+            userId: res.data.localId
+          })
+        })
         .catch(error => console.log(error));
+    },
+    storeUser({commit, state}, userData){
+      if(!state.idToken){
+        return
+      }
+      globalAxios.post('/users.json' + '?auth=' +state.idToken , userData).then(res=> console.log(res)).catch(error => console.log(error));
+    },
+    fetchUser({commit, state}){
+      globalAxios.get('/users.json' + '?auth=' +state.idToken)
+        .then(res => {
+          console.log(res)
+          const data = res.data
+          const users = []
+          for (let key in data) {
+            const user = data[key]
+            user.id = key
+            users.push(user)
+          }
+          console.log(users);
+          commit('storeUser', users[0]);
+          //this.email = users[0].email
+        })
+        .catch(error => console.log(error))
     }
 
   },
   getters: {
-
+    user (state){
+      return state.user;
+    }
   }
 })
